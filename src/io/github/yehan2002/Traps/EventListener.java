f6405f49e22ea6676e75469b8eb5fece009537de
zlwkrdmsska <47001449+zlwkrdmsska@users.noreply.github.com>
2019-01-24 23:58:27 +0900

    Update EventListener.java

--- src/io/github/yehan2002/Traps/EventListener.java
+++ src/io/github/yehan2002/Traps/EventListener.java
@@ -197,7 +197,7 @@ public class EventListener implements Listener{
                     }
 
                 } else {
-                    e.setExpToDrop(50);
+                    e.setExpToDrop(5);
                 }
                 config.set(getIdentifier(e.getBlock()),null);
                 saveConfig();
